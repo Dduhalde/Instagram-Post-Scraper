59857a901276b77c7b5534c3ad6d2909f405a1a6
Dduhalde <dduhalde@hotmail.com>
2024-06-19 14:12:41 -0400

    Finish Scrap Post

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import csv
+import os
+from dotenv import load_dotenv
 
-# Iniciar drivers de selenium
+# Post class
+class Post:
+    def __init__(self, post_info, date, like):
+        self.post_info = post_info
+        self.date = date
+        self.like = like
+
+# Start the driver
 def iniciar_driver():
     chrome_options = Options()
     chrome_options.add_argument('--disable-dev-shm-usage')
@@ -14,42 +24,58 @@ def iniciar_driver():
     driver = webdriver.Chrome(options=chrome_options)
     return driver
 
-# Pausar ejecución del programa
+# Sleep function
 def mySleep(nTimeOut):
     nTimeInit = time.time()
     nTimeDifference = time.time() - nTimeInit
     while nTimeDifference < nTimeOut:
         nTimeDifference = time.time() - nTimeInit
 
-# Scrap de instagram
-def scrap_instagram(cuenta, driver):
-    # Abrir perfil de instagram
-    driver.get('https://www.instagram.com/' + cuenta)
+# Instagram Scrap
+def instagramScrap(accountToScrap, driver, postAmountToScrap):
+    # Open the profile account
+    driver.get('https://www.instagram.com/' + accountToScrap)
     mySleep(5)
-    # Conseguir el primer post
+    # Get the first post
     post = driver.find_element(By.CSS_SELECTOR, 'div.x1lliihq.x1n2onr6.xh8yej3.x4gyw5p.xfllauq.xo2y696.x11i5rnm.x2pgyrj')
     post.click()
-    # Recorrer los post
-    for i in range(5):
-        mySleep(2)
+    # Create a list to store the posts
+    posts = []
+    # Loop to get the posts
+    for _ in range(postAmountToScrap):
+        if _ % 10 == 0:
+            print(_)
+        mySleep(1)
         html = driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
-        html_file = open(f"{i}_html.html", "w", encoding='utf-8')
-        html_file.write(html)
-        html_file.close()
-        soup_file = open(f"{i}_soup.txt", "w", encoding='utf-8')
-        soup_file.write(str(soup))
-        soup_file.close()
-        # Conseguir la fecha del post
-        time_elements = soup.find_all('time', datetime=True)
-        print(time_elements[-1].get('datetime'))
-        # Apretar el botón de siguiente
+        # Get post information
+        postInfo = soup.find_all('div', class_='_a9zs')
+        try:
+            postInfo = postInfo[0].text
+        except:
+            postInfo = 'No post info'
+        # Get the date
+        timeElements = soup.find_all('time', datetime=True)
+        try:
+            date = timeElements[-1].get('datetime')
+        except:
+            date = 'No date'
+        # Get the likes
+        likes = soup.find_all('span', class_='html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs')
+        try:
+            likes = likes[-1].text
+        except:
+            likes = 0
+        # Create a post object
+        post = Post(postInfo, date, likes)
+        # Append the post to the list
+        posts.append(post)
+        # Click the next button
         button = driver.find_element(By.CSS_SELECTOR, 'div._aaqg._aaqh button._abl-')
         button.click()
-    driver.quit()
-    return soup
+    return posts
 
-# Iniciar sesión en instagram
+# Login to Instagram
 def iniciar_sesion(driver, user, password):
     driver.get('https://www.instagram.com/')
     mySleep(5)  # Wait for 5 seconds
@@ -61,9 +87,30 @@ def iniciar_sesion(driver, user, password):
     mySleep(5)  # Wait for 5 seconds after sending the password
     return driver
 
+# Convert list to csv
+def listToCsv(list, filename):
+    with open(filename, 'w', encoding='utf-8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Post Info', 'Date', 'Likes'])
+        for item in list:
+            writer.writerow([item.post_info, item.date, item.like])
+
 if __name__ == '__main__':
-    cuenta = 'sanmiguel_cl'
+    # Start the driver
     driver = iniciar_driver()
-    driver = iniciar_sesion(driver, "diegoimlp@gmail.com", "Diegoimlp14")
-    soup = scrap_instagram(cuenta, driver)
-    print(soup)
+    # Load environment variables from .env file
+    load_dotenv()
+    # Get the values from environment variables
+    user = os.getenv("INSTAGRAM_USER")
+    password = os.getenv("INSTAGRAM_PASSWORD")
+    driver = iniciar_sesion(driver, user, password)
+    # Read the accounts to scrap
+    with open('accounts_to_scrap.txt', 'r') as file:
+        accounts = file.read().splitlines()
+    # Set the amount of posts to scrap
+    postAmountToScrap = 3000
+    # Scrap the accounts
+    for account in accounts:
+        posts = instagramScrap(account, driver, postAmountToScrap)
+        listToCsv(posts, f'{account}.csv')
+    driver.quit()
